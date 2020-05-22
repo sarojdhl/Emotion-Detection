c7f4668550c454732ac9f1a0e24aaae93094b96c
kshitizregmi61 <kshitizregmi61@gmail.com>
2020-05-22 11:25:28 +0545

    Updates on project

--- api/app.py
+++ api/app.py
@@ -6,25 +6,23 @@ from flask import Flask, request, render_template
 from flask_restful import Api, Resource
 app = Flask(__name__)
 apis = Api(app)
-# from src.db import *
 import numpy as np
-from src.db import load_saved_model_from_db
 from src.preprocessing.preprocessing import preprocessing
+#from src.db import load_saved_model_from_db
 import pickle
 from src.data.clean_data import get_value
-# call a function to recomend
-from src.db import *
+
 
 
 def predict_type(msg):
     print("Message inpredict_type: ", msg)
     # Call a model to predict message
-    model = load_saved_model_from_db(model_name = 'SGD_clf',collection_name='model')
+    model  = pickle.load(open('../checkpoint/model/01SGD.pkl', 'rb'))  #load_saved_model_from_db(model_name = 'SGD_clf',collection_name='model')
     # mess = list(msg)
     # print(type(mess))
     cleaned_data = [get_value(msg)]
     # Load preprocessor from db
-    preprocessor = load_saved_model_from_db(model_name = 'preprocessor',collection_name='prep')
+    preprocessor = pickle.load(open('../checkpoint/preprocessor/preprocessor.pkl','rb'))#load_saved_model_from_db(model_name = 'preprocessor',collection_name='prep')
 
     out_vect = preprocessor.transform(cleaned_data)
 
@@ -33,16 +31,6 @@ def predict_type(msg):
     return output
 
 
-# class Rec(Resource):
-#     # def post(self):
-#     #     # get posted data
-#     #     data = request.get_json()
-#     #     print(data)
-#     #     msg = data['message']
-#     #     # preprocessed_data= preprocessing.train_preprocessing(msg)
-#     #     predicted_emotion = predict_type(msg)
-#     #     output = {"Message": msg, "Emotion": predicted_emotion[0]}
-#     #     return jsonify(output)
 
 @app.route('/', methods=['POST','GET'])
 def home():
@@ -58,7 +46,7 @@ def home():
         predicted_emotion = predict_type(msg)
         output = {"Message": msg, "Emotion": predicted_emotion[0]}
         print (output)
-        predicted_data(msg,predicted_emotion[0])
+        #predicted_data(msg,predicted_emotion[0])
         return render_template('output.htm',data=output["Message"], emotion=output["Emotion"])
 
 
